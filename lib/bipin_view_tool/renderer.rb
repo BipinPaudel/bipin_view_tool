e98e1941e7f7d680dc17102e7b81d0a8b840f80c
Bipin Paudel <bipinpaudel@Bipins-MacBook-Pro.local>
2020-08-02 10:46:47 +0545

    Dev tool initial commit

--- lib/bipin_view_tool/renderer.rb
+++ lib/bipin_view_tool/renderer.rb
@@ -1,7 +1,9 @@
 module BipinViewTool
   class Error < StandardError; end
   # Your code goes here...
-  def self.copyright name, msg
-    "&copy; #{Time.now.year} | <b> #{name} <b> #{msg}".html_safe
-  end
+  class Renderer
+	  def self.copyright name, msg
+	    "&copy; #{Time.now.year} | <b> #{name} <b> #{msg}".html_safe
+	  end
+	end
 end
